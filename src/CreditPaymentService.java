c14107381f59b6867a3dcd69d64fe2ce37d2c0cf
Krasnikov <krastist@yandex.ru>
2023-04-21 22:48:18 +0500

    Change credit service

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -4,9 +4,7 @@ public class CreditPaymentService {
         double monthlyRate = interestRate / 12 / 100; //Месячная процентная ставка
         double a = 1 + monthlyRate;
         double a1 = Math.pow(a, loanTermMonth);
-        String a2 = String.format("%.10f", a1); //
-        System.out.println();
-        System.out.println();
+        String a2 = String.format("%.10f", a1); //Как внедрить в формулу округленное число что бы получился верный ответ?
         double monthlyPayment = (loanAmount * monthlyRate * a1) / (a1 - 1);
         return (int) monthlyPayment;
     }
